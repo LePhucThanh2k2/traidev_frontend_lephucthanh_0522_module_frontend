a3f143791c071d640c1f903d3aa817582c2d90b0
Nguyen Van Linh <nvlinh17041992@gmail.com>
2022-06-30 09:27:42 +0700

    fix reading book

--- exercise-js/b03-content/js/app.js
+++ exercise-js/b03-content/js/app.js
@@ -3,60 +3,62 @@ var eleTextAlign = document.querySelector(".textAlign");
 var eleContent = document.getElementById("content");
 var fontSize = Number.parseInt(
   window.getComputedStyle(eleContent, null).getPropertyValue("font-size")
-  // window.getComputedStyle(eleContent, null).fontSize
 );
 
 // create data to save localStorage
-var data = {};
-// set font size default for content
-eleContent.style.fontSize = fontSize + "px";
 
 // Set Data To localStorage
-window.onload = function setDataFromLocalStorage() {
-  var dataLocal = JSON.parse(localStorage.getItem("dataContent"));
-  eleContent.style.backgroundColor = dataLocal.backgroundColor;
-  eleContent.style.fontSize = dataLocal.fontSize + "px";
-  eleContent.style.lineHeight = dataLocal.lineHeight;
-  eleContent.style.textAlign = dataLocal.textAlign;
-  eleLineHeight.value = dataLocal.lineHeight;
-  eleTextAlign.value = dataLocal.textAlign;
-};
+// window.onload = function setDataFromLocalStorage() {
+//   var dataLocal = JSON.parse(localStorage.getItem("dataContent"));
+//   eleContent.style.backgroundColor = dataLocal.backgroundColor;
+//   eleContent.style.fontSize = dataLocal.fontSize + "px";
+//   eleContent.style.lineHeight = dataLocal.lineHeight;
+//   eleContent.style.textAlign = dataLocal.textAlign;
+//   eleLineHeight.value = dataLocal.lineHeight;
+//   eleTextAlign.value = dataLocal.textAlign;
+// };
+
+let config = JSON.parse(localStorage.getItem("dataContent")) || {};
+for (const property in config) {
+  eleContent.style[property] = config[property];
+}
 
 // Add Event Click
 document.addEventListener("click", function (e) {
   let ele = e.target;
-  eleContent.style.fontSize = fontSize + "px";
 
   if (ele.classList.contains("btn-background")) {
     var color = ele.dataset.value;
     eleContent.style.backgroundColor = color;
-    data.backgroundColor = color;
+    saveSetting('backgroundColor', color);
   }
 
-  if (ele.classList.contains("decrease")) {
-    fontSize -= 2;
+  if (ele.classList.contains("btn-font-size")) {
+    let fontSize = parseInt(window.getComputedStyle(eleContent).getPropertyValue("font-size"));
+    if (ele.classList.contains("decrease")) {
+      fontSize -= 2;
+    } else {
+      fontSize += 2;
+    }
     eleContent.style.fontSize = fontSize + "px";
-    data.fontSize = fontSize;
+    saveSetting('fontSize', fontSize + 'px');
   }
-
-  if (ele.classList.contains("increase")) {
-    fontSize += 2;
-    eleContent.style.fontSize = fontSize + "px";
-    data.fontSize = fontSize;
-  }
-  localStorage.setItem("dataContent", JSON.stringify(data));
 });
 
-//Add Event Change
-document.addEventListener("change", function (e) {
-  let ele = e.target;
-  if (ele.classList.contains("lineHeight")) {
-    eleContent.style.lineHeight = eleLineHeight.value;
-    data.lineHeight = eleLineHeight.value;
-  }
-  if (ele.classList.contains("textAlign")) {
-    eleContent.style.textAlign = eleTextAlign.value;
-    data.textAlign = eleTextAlign.value;
-  }
-  localStorage.setItem("dataContent", JSON.stringify(data));
+eleLineHeight.addEventListener('change', function () {
+  let value = eleLineHeight.value
+  eleContent.style.lineHeight = value;
+  saveSetting('lineHeight', value);
 });
+
+eleTextAlign.addEventListener('change', function () {
+  let value = eleTextAlign.value
+  eleContent.style.textAlign = value;
+  saveSetting('textAlign', value);
+});
+
+function saveSetting(key, value) {
+  let data = JSON.parse(localStorage.getItem('dataContent')) || {};
+  data[key] = value;
+  localStorage.setItem("dataContent", JSON.stringify(data));
+}
