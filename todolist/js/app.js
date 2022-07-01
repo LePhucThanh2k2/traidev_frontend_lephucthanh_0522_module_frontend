16912e01a865683fd1f7117515405c43f2bfa08e
binniac <agencysocialmediagenz@gmail.com>
2022-07-01 10:23:28 +0700

    answer homework with todolist

--- todolist/js/app.js
+++ todolist/js/app.js
@@ -60,31 +60,38 @@ const inputSearch = document.querySelector(".input-search");
 const select = document.querySelector(".select");
 const sortList = document.querySelectorAll(".dropdown-item");
 const showSortFeature = document.querySelector(".showSortFeature");
-let item = "";
+
+// idItem to compare with submit
 let idItem = 0;
-renderItems(todos);
+
+// set data-isedit for input submit
 submit.dataset.isedit = "false";
+
+let configData = JSON.parse(localStorage.getItem("dataToDoList")) || todos;
+renderItems(configData);
 
 // EVENTS
 document.addEventListener("click", (e) => {
   ele = e.target;
 
+  // DELETE
   if (ele.classList.contains("delete")) {
     if (confirm("are you sure?")) {
       let id = ele.dataset.id;
-      todos = todos.filter(function (obj) {
+      configData = configData.filter(function (obj) {
         return obj.id !== id;
       });
-
-      renderItems(todos);
+      saveDataAndDisplay();
     }
   }
 
+  // EDIT
   if (ele.classList.contains("edit")) {
-    // set isEdit= true for submit
+    // set isEdit = true for submit
     submit.dataset.isedit = "true";
+
     idItem = ele.dataset.id;
-    todos.forEach((obj) => {
+    configData.forEach((obj) => {
       if (obj.id === idItem) {
         // display data out form
         inputAddTask.value = obj.name;
@@ -94,42 +101,27 @@ document.addEventListener("click", (e) => {
   }
 });
 
-// Event Search
+// Handle Search Feature
 inputSearch.addEventListener("keyup", () => {
   const keys = inputSearch.value.toLowerCase();
+  // if the form input search is empty return original data
   if (keys === "") {
-    renderItems(todos);
+    saveDataAndDisplay();
     return;
   }
+  // reset form
   let itemList = "";
   let newName = "";
 
-  for (let i = 0; i < todos.length; i++) {
+  for (let i = 0; i < configData.length; i++) {
     const keyWords = new RegExp(keys, "gim");
-    const element = todos[i];
+    const element = configData[i];
     const haveKeyword = element.name.toLowerCase().includes(keys);
     if (haveKeyword) {
       newName = element.name.replace(keyWords, function (match) {
         return "<mark>" + match + "</mark>";
       });
-      itemList += `
-      <tr>
-        <th>${i + 1}</th>
-        <td>${newName}</td>
-        <td>
-        ${handleBadge(element.level)}
-          
-        </td>
-        <td>
-          <button class="btn btn-warning btn-sm edit"data-id="${
-            element.id
-          }" >Edit</button>
-          <button class="btn btn-danger btn-sm delete" data-id ="${
-            element.id
-          }">Delete</button>
-        </td>
-      </tr>
-    `;
+      itemList += createItem(element, i, newName);
     }
   }
   tbody.innerHTML = itemList;
@@ -137,14 +129,20 @@ inputSearch.addEventListener("keyup", () => {
 
 // Event Add Task
 submit.addEventListener("click", () => {
+  // check is edit or add
+  // if isedit = true is edit
   if (submit.dataset.isedit === "true") {
-    todos.forEach((obj) => {
+    configData.forEach((obj) => {
       if (obj.id === idItem) {
-        obj.name = inputAddTask.value;
-        obj.level = parseInt(select.value);
+        if (inputAddTask.value !== "") {
+          obj.name = inputAddTask.value;
+          obj.level = parseInt(select.value);
+        } else {
+          alert("Input Value Can't Be Empty");
+        }
       }
     });
-    renderItems(todos);
+    saveDataAndDisplay();
 
     // reset submit
     submit.dataset.isedit = "false";
@@ -154,8 +152,8 @@ submit.addEventListener("click", () => {
     if (inputAddTask.value !== "") {
       data.name = inputAddTask.value;
       data.level = parseInt(select.value);
-      todos.unshift(data);
-      renderItems(todos);
+      configData.unshift(data);
+      saveDataAndDisplay();
     } else {
       alert("Please Enter Value On Input");
     }
@@ -165,12 +163,13 @@ submit.addEventListener("click", () => {
   inputAddTask.value = "";
   select.value = 1;
 });
+
 // Event Sort
 sortList.forEach((item) => {
   item.addEventListener("click", () => {
     showSortFeature.textContent = item.textContent.toUpperCase();
+    // item.dataset.value is the name of the feature(name-asc|name-desc|level-asc|level-desc)
     sortFeature(item.dataset.value);
-    console.log(item.dataset.value);
   });
 });
 
@@ -194,24 +193,7 @@ function handleBadge(n) {
 function renderItems(items) {
   let item = "";
   items.forEach((obj, idx) => {
-    item += `
-      <tr>
-        <th>${idx + 1}</th>
-        <td>${obj.name}</td>
-        <td>
-        ${handleBadge(obj.level)}
-          
-        </td>
-        <td>
-          <button class="btn btn-warning btn-sm edit"data-id="${
-            obj.id
-          }" >Edit</button>
-          <button class="btn btn-danger btn-sm delete" data-id ="${
-            obj.id
-          }">Delete</button>
-        </td>
-      </tr>
-    `;
+    item += createItem(obj, idx, obj.name);
     tbody.innerHTML = item;
   });
 }
@@ -226,9 +208,10 @@ function createId(length) {
   }
   return result;
 }
+
 function sortFeature(value) {
   if (value === "name-asc") {
-    todos.sort(function (a, b) {
+    configData.sort(function (a, b) {
       const nameA = a.name.toUpperCase(); // ignore upper and lowercase
       const nameB = b.name.toUpperCase(); // ignore upper and lowercase
       if (nameA < nameB) {
@@ -239,10 +222,10 @@ function sortFeature(value) {
       }
       return 0;
     });
-    renderItems(todos);
   }
+
   if (value === "name-desc") {
-    todos
+    configData
       .sort(function (a, b) {
         const nameA = a.name.toUpperCase(); // ignore upper and lowercase
         const nameB = b.name.toUpperCase(); // ignore upper and lowercase
@@ -255,10 +238,10 @@ function sortFeature(value) {
         return 0;
       })
       .reverse();
-    renderItems(todos);
   }
+
   if (value === "level-asc") {
-    todos.sort(function (a, b) {
+    configData.sort(function (a, b) {
       const levelA = a.level; // ignore upper and lowercase
       const levelB = b.level; // ignore upper and lowercase
       if (levelA < levelB) {
@@ -269,10 +252,10 @@ function sortFeature(value) {
       }
       return 0;
     });
-    renderItems(todos);
   }
+
   if (value === "level-desc") {
-    todos
+    configData
       .sort(function (a, b) {
         const levelA = a.level; // ignore upper and lowercase
         const levelB = b.level; // ignore upper and lowercase
@@ -285,6 +268,32 @@ function sortFeature(value) {
         return 0;
       })
       .reverse();
-    renderItems(todos);
   }
+  saveDataAndDisplay();
+}
+
+function saveDataAndDisplay() {
+  localStorage.setItem("dataToDoList", JSON.stringify(configData));
+  renderItems(configData);
+}
+
+function createItem(obj, idx, name) {
+  return `
+  <tr>
+    <th>${idx + 1}</th>
+    <td>${name}</td>
+    <td>
+    ${handleBadge(obj.level)}
+      
+    </td>
+    <td>
+      <button class="btn btn-warning btn-sm edit"data-id="${
+        obj.id
+      }" >Edit</button>
+      <button class="btn btn-danger btn-sm delete" data-id ="${
+        obj.id
+      }">Delete</button>
+    </td>
+  </tr>
+`;
 }
