53a22c0eee34358c07d1893dc16a1bdbd56d9c24
Linh Nguyen <nvlinh17041992@gmail.com>
2022-08-08 11:27:13 +0700

    fix

--- news/js/index.js
+++ news/js/index.js
@@ -114,22 +114,21 @@ function renderLatestNews() {
     let htmlThird = "";
     data.forEach((item, idx) => {
       if (idx <= 1) {
-        htmlPrimary += renderLatestNewsPrimaryItem(item);
-      } else if (idx <= 3 && idx > 1) {
-        htmlSecondary += renderLatestNewsSecondaryItem(item);
+        htmlPrimary += `<div class="col-lg-6">${renderLatestNewsPrimaryItem(item)}</div>`;
+      } else if (idx <= 3) {
+        htmlSecondary += `<div class="col-lg-6">${renderLatestNewsPrimaryItem(item, false)}</div>`;
       } else {
         htmlThird += renderLatestNewsThirdItem(item);
       }
     });
     eleLatestNews.innerHTML = `
-        <div class="col-12">
-        <div class="section-title">
-            <h4 class="m-0 text-uppercase font-weight-bold">Latest News</h4>
-            <a class="text-secondary font-weight-medium text-decoration-none" href="">View All</a>
-        </div>
+    <div class="col-12">
+      <div class="section-title">
+        <h4 class="m-0 text-uppercase font-weight-bold">Latest News</h4>
+        <a class="text-secondary font-weight-medium text-decoration-none" href="">View All</a>
+      </div>
     </div>
-    ${htmlPrimary}${htmlSecondary}${htmlThird}
-        `;
+    ${htmlPrimary} ${htmlSecondary} ${htmlThird}`;
   });
 }
 
@@ -161,83 +160,51 @@ function renderFeatureNewsItem(data) {
    </div>
 </div>`;
 }
-function renderLatestNewsPrimaryItem(data) {
-  return `
-  <div class="col-lg-6">
+
+function renderLatestNewsPrimaryItem(data, isShowDesc = true) {
+  const desc = isShowDesc ? `<p class="m-0 desc">${data.description}</p>` : '';
+  return /* html */`
     <div class="position-relative mb-3">
-        <img class="img-fluid w-100" src="${data.thumb}" style="object-fit: cover;">
-        <div class="bg-white border border-top-0 p-4">
-            <div class="mb-2">
-                <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2 category"
-                    href="">${data.category.name}</a>
-                <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
-            </div>
-            <a class="h4 d-block mb-3 text-secondary text-uppercase font-weight-bold title"
-                href="">${data.title}</a>
-            <p class="m-0 desc">${data.description}</p>
+      <img class="img-fluid w-100" src="${data.thumb}" style="object-fit: cover;">
+      <div class="bg-white border border-top-0 p-4">
+        <div class="mb-2">
+          <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2 category"
+              href="">${data.category.name}</a>
+          <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
         </div>
-        <div class="d-flex justify-content-between bg-white border border-top-0 p-4">
-            <div class="d-flex align-items-center">
-                <img class="rounded-circle mr-2" src="img/user.jpg" width="25" height="25"
-                    alt="">
-                <small class="author">John Doe</small>
-            </div>
-            <div class="d-flex align-items-center">
-                <small class="ml-3"><i class="far fa-eye mr-2"></i>12345</small>
-                <small class="ml-3"><i class="far fa-comment mr-2"></i>123</small>
-            </div>
+        <a class="h4 d-block mb-3 text-secondary text-uppercase font-weight-bold title"
+            href="">${data.title}</a>
+        ${desc}
+      </div>
+      <div class="d-flex justify-content-between bg-white border border-top-0 p-4">
+        <div class="d-flex align-items-center">
+          <img class="rounded-circle mr-2" src="img/user.jpg" width="25" height="25"
+              alt="">
+          <small class="author">John Doe</small>
         </div>
-    </div>
-</div>
-  `;
+        <div class="d-flex align-items-center">
+          <small class="ml-3"><i class="far fa-eye mr-2"></i>12345</small>
+          <small class="ml-3"><i class="far fa-comment mr-2"></i>123</small>
+        </div>
+      </div>
+    </div>`;
 }
 
-function renderLatestNewsSecondaryItem(data) {
-  return `
-  <div class="col-lg-6 ">
-    <div class="position-relative mb-3">
-        <img class="img-fluid w-100" src="img/news-700x435-4.jpg" style="object-fit: cover;">
-        <div class="bg-white border border-top-0 p-4">
-            <div class="mb-2">
-                <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2 category"
-                    href="">${data.category.name}</a>
-                <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
-            </div>
-            <a class="h4 d-block mb-0 text-secondary text-uppercase font-weight-bold title"
-                href="">${data.title}</a>
-        </div>
-        <div class="d-flex justify-content-between bg-white border border-top-0 p-4">
-            <div class="d-flex align-items-center">
-                <img class="rounded-circle mr-2" src="img/user.jpg" width="25" height="25"
-                    alt="">
-                <small class="author">John Doe</small>
-            </div>
-            <div class="d-flex align-items-center">
-                <small class="ml-3"><i class="far fa-eye mr-2"></i>12345</small>
-                <small class="ml-3"><i class="far fa-comment mr-2"></i>123</small>
-            </div>
-        </div>
-    </div>
-</div>
-  `;
-}
 function renderLatestNewsThirdItem(data) {
   return `
   <div class="col-lg-6 latest-news-third">
     <div class="d-flex align-items-center bg-white mb-3" style="height: 110px;">
       <img class="img-fluid" src="img/news-110x110-1.jpg" alt="">
-      <div
-          class="w-100 h-100 px-3 d-flex flex-column justify-content-center border border-left-0">
-          <div class="mb-2">
-              <a class="badge badge-primary text-uppercase font-weight-semi-bold p-1 mr-2 category"
-                  href="">${data.category.name}</a>
-              <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
-          </div>
-          <a class="h6 m-0 text-secondary text-uppercase font-weight-bold title" href="">${data.title}</a>
+      <div class="w-100 h-100 px-3 d-flex flex-column justify-content-center border border-left-0">
+        <div class="mb-2">
+          <a class="badge badge-primary text-uppercase font-weight-semi-bold p-1 mr-2 category"
+              href="">${data.category.name}</a>
+          <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
+        </div>
+        <a class="h6 m-0 text-secondary text-uppercase font-weight-bold title" href="">${data.title}</a>
       </div>
-  </div>
-</div>
-  `;
+    </div>
+  </div>`;
 }
 // ASYNC FUNCTION
 async function getCategoriesNews(offset = 0, limit = 15) {
