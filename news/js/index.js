71febb8efc70839559bfdbb133c27ca0747d6352
binniac <agencysocialmediagenz@gmail.com>
2022-08-08 11:18:22 +0700

    done

--- news/js/index.js
+++ news/js/index.js
@@ -2,70 +2,15 @@ const API = axios.create({
   baseURL: "http://apiforlearning.zendvn.com/api/",
 });
 
-// Main news
-const categoryMainNews = document.querySelectorAll(".main-news .category");
-const dateMainNews = document.querySelectorAll(".main-news .date");
-const titleMainNews = document.querySelectorAll(".main-news .title");
-// Featured news
-const categoryFeaturedNews = document.querySelectorAll(
-  ".feature-news .category"
-);
-const dateFeaturedNews = document.querySelectorAll(".feature-news .date");
-const titleFeaturedNews = document.querySelectorAll(".feature-news .title");
-// Latest news primary
-const categoryLatestNewsPrimary = document.querySelectorAll(
-  ".latest-news-primary .category"
-);
-const dateLatestNewsPrimary = document.querySelectorAll(
-  ".latest-news-primary .date"
-);
-const titleLatestNewsPrimary = document.querySelectorAll(
-  ".latest-news-primary .title"
-);
-const descLatestNewsPrimary = document.querySelectorAll(
-  ".latest-news-primary .desc"
-);
-// Latest news secondary
-const categoryLatestNewsSecondary = document.querySelectorAll(
-  ".latest-news-secondary .category"
-);
-const dateLatestNewsSecondary = document.querySelectorAll(
-  ".latest-news-secondary .date"
-);
-const titleLatestNewsSecondary = document.querySelectorAll(
-  ".latest-news-secondary .title"
-);
-// Latest news third
-const categoryLatestNewsThird = document.querySelectorAll(
-  ".latest-news-third .category"
-);
-const dateLatestNewsThird = document.querySelectorAll(
-  ".latest-news-third .date"
-);
-const titleLatestNewsThird = document.querySelectorAll(
-  ".latest-news-third .title"
-);
-// trending news
-const categoryTrendingThird = document.querySelectorAll(
-  ".trending-news .category"
-);
-const dateTrendingThird = document.querySelectorAll(".trending-news .date");
-const titleTrendingThird = document.querySelectorAll(".trending-news .title");
-// Tags
-const eleTags = document.querySelector(".tags");
-// categories
-const eleCategories = document.querySelector(".footer-categories");
-// Popular
-const categoryPopular = document.querySelectorAll(".popular .category");
-const datePopular = document.querySelectorAll(".popular .date");
-const titlePopular = document.querySelectorAll(".popular .title");
-
 const eleMenu = document.getElementById("menu");
 const eleTopMenuLeft = document.querySelector("#top-news-left");
 const eleTopMenuRight = document.querySelector("#top-news-right");
+const eleFeatureNews = document.getElementById("feature-news");
+const eleLatestNews = document.getElementById("latest-news");
 renderMenu();
 renderTopNews();
-
+renderFeatureNews();
+renderLatestNews();
 // ------------------------------------
 // RENDER FUNCTION
 function renderMenu() {
@@ -103,7 +48,11 @@ function renderTopNews() {
       if (idx <= 2) {
         htmlTopMenuLeft += renderTopNewsItem(item, 500);
       } else {
-        htmlTopMenuRight += `<div class="col-md-6 px-0"> ${renderTopNewsItem(item, 250, "w-100")} </div>`;
+        htmlTopMenuRight += `<div class="col-md-6 px-0"> ${renderTopNewsItem(
+          item,
+          250,
+          "w-100"
+        )} </div>`;
       }
     });
     eleTopMenuLeft.innerHTML = htmlTopMenuLeft;
@@ -116,6 +65,71 @@ function renderTopNews() {
       loop: true,
       center: true,
     });
+  });
+}
+function renderFeatureNews() {
+  getArticlesTop(7).then((res) => {
+    const data = res.data;
+    let htmlFeatureNews = "";
+    data.forEach((item) => {
+      htmlFeatureNews += renderFeatureNewsItem(item);
+    });
+    eleFeatureNews.innerHTML = htmlFeatureNews;
+    $(".carousel-item-4").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1000,
+      margin: 30,
+      dots: false,
+      loop: true,
+      nav: true,
+      navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+      ],
+      responsive: {
+        0: {
+          items: 1,
+        },
+        576: {
+          items: 1,
+        },
+        768: {
+          items: 2,
+        },
+        992: {
+          items: 3,
+        },
+        1200: {
+          items: 4,
+        },
+      },
+    });
+  });
+}
+function renderLatestNews() {
+  getArticles().then((res) => {
+    const data = res.data;
+    let htmlPrimary = "";
+    let htmlSecondary = "";
+    let htmlThird = "";
+    data.forEach((item, idx) => {
+      if (idx <= 1) {
+        htmlPrimary += renderLatestNewsPrimaryItem(item);
+      } else if (idx <= 3 && idx > 1) {
+        htmlSecondary += renderLatestNewsSecondaryItem(item);
+      } else {
+        htmlThird += renderLatestNewsThirdItem(item);
+      }
+    });
+    eleLatestNews.innerHTML = `
+        <div class="col-12">
+        <div class="section-title">
+            <h4 class="m-0 text-uppercase font-weight-bold">Latest News</h4>
+            <a class="text-secondary font-weight-medium text-decoration-none" href="">View All</a>
+        </div>
+    </div>
+    ${htmlPrimary}${htmlSecondary}${htmlThird}
+        `;
   });
 }
 
@@ -133,6 +147,98 @@ function renderTopNewsItem(data, height = 200, classAttrImg = "") {
   </div>`;
 }
 
+function renderFeatureNewsItem(data) {
+  return ` 
+  <div class="position-relative overflow-hidden feature-news" style="height: 300px;">
+   <img class="img-fluid h-100" src="${data.thumb}" style="object-fit: cover;">
+   <div class="overlay">
+       <div class="mb-2">
+           <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2 category"
+               href="">${data.category.name}</a>
+           <a class="text-white" href=""><small class="date">${data.publish_date}</small></a>
+       </div>
+       <a class="h6 m-0 text-white text-uppercase font-weight-semi-bold title" href="">${data.title}</a>
+   </div>
+</div>`;
+}
+function renderLatestNewsPrimaryItem(data) {
+  return `
+  <div class="col-lg-6">
+    <div class="position-relative mb-3">
+        <img class="img-fluid w-100" src="${data.thumb}" style="object-fit: cover;">
+        <div class="bg-white border border-top-0 p-4">
+            <div class="mb-2">
+                <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2 category"
+                    href="">${data.category.name}</a>
+                <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
+            </div>
+            <a class="h4 d-block mb-3 text-secondary text-uppercase font-weight-bold title"
+                href="">${data.title}</a>
+            <p class="m-0 desc">${data.description}</p>
+        </div>
+        <div class="d-flex justify-content-between bg-white border border-top-0 p-4">
+            <div class="d-flex align-items-center">
+                <img class="rounded-circle mr-2" src="img/user.jpg" width="25" height="25"
+                    alt="">
+                <small class="author">John Doe</small>
+            </div>
+            <div class="d-flex align-items-center">
+                <small class="ml-3"><i class="far fa-eye mr-2"></i>12345</small>
+                <small class="ml-3"><i class="far fa-comment mr-2"></i>123</small>
+            </div>
+        </div>
+    </div>
+</div>
+  `;
+}
+
+function renderLatestNewsSecondaryItem(data) {
+  return `
+  <div class="col-lg-6 ">
+    <div class="position-relative mb-3">
+        <img class="img-fluid w-100" src="img/news-700x435-4.jpg" style="object-fit: cover;">
+        <div class="bg-white border border-top-0 p-4">
+            <div class="mb-2">
+                <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2 category"
+                    href="">${data.category.name}</a>
+                <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
+            </div>
+            <a class="h4 d-block mb-0 text-secondary text-uppercase font-weight-bold title"
+                href="">${data.title}</a>
+        </div>
+        <div class="d-flex justify-content-between bg-white border border-top-0 p-4">
+            <div class="d-flex align-items-center">
+                <img class="rounded-circle mr-2" src="img/user.jpg" width="25" height="25"
+                    alt="">
+                <small class="author">John Doe</small>
+            </div>
+            <div class="d-flex align-items-center">
+                <small class="ml-3"><i class="far fa-eye mr-2"></i>12345</small>
+                <small class="ml-3"><i class="far fa-comment mr-2"></i>123</small>
+            </div>
+        </div>
+    </div>
+</div>
+  `;
+}
+function renderLatestNewsThirdItem(data) {
+  return `
+  <div class="col-lg-6 latest-news-third">
+    <div class="d-flex align-items-center bg-white mb-3" style="height: 110px;">
+      <img class="img-fluid" src="img/news-110x110-1.jpg" alt="">
+      <div
+          class="w-100 h-100 px-3 d-flex flex-column justify-content-center border border-left-0">
+          <div class="mb-2">
+              <a class="badge badge-primary text-uppercase font-weight-semi-bold p-1 mr-2 category"
+                  href="">${data.category.name}</a>
+              <a class="text-body" href=""><small class="date">${data.updated_at}</small></a>
+          </div>
+          <a class="h6 m-0 text-secondary text-uppercase font-weight-bold title" href="">${data.title}</a>
+      </div>
+  </div>
+</div>
+  `;
+}
 // ASYNC FUNCTION
 async function getCategoriesNews(offset = 0, limit = 15) {
   return await API.get("categories_news", {
@@ -151,128 +257,11 @@ async function getArticlesTop(limit = 10) {
   });
 }
 
-async function getArticlesById(offset = 0, limit = 64, id) {
-  let items = null;
-  await API.get(`categories_news/${id}/articles`, {
+async function getArticles(offset = 0, limit = 12) {
+  return await API.get(`articles`, {
     params: {
       offset: offset,
       limit: limit,
     },
-  }).then((res) => {
-    items = res.data;
   });
-  return items;
 }
-
-// getCategoriesNews()
-//   .then((res) => {
-//     console.log(res);
-//     let dropdownItems = "";
-//     let strTagName = "";
-//     let strCategories = "";
-//     for (const key in res) {
-//       const categories = res[key].name;
-//       strTagName += ` <a href="" class="btn btn-sm btn-outline-secondary m-1">${categories}</a>`;
-//       strCategories += ` <a href="" class="btn btn-sm btn-secondary m-1">${categories}</a>`;
-//     }
-//     for (const key in res) {
-//       const item = res[key];
-//       if (key < navItemList.length) {
-//         navItemList[key].textContent = item.name;
-//         navItemList[key].dataset.id = item.id;
-//       } else {
-//         dropdownItems += ` <a href="#" class="dropdown-item" data-id="${item.id}">${item.name}</a>`;
-//       }
-//     }
-//     eleTags.innerHTML = strTagName;
-//     eleCategories.innerHTML = strCategories;
-//     dropdownMenu.innerHTML = dropdownItems;
-//     idCategory = categoriesActive.dataset.id;
-//     return getArticlesById(0, 15, idCategory);
-//   })
-//   .then((res) => {
-//     // Main News
-//     for (let i = 0; i < categoryMainNews.length; i++) {
-//       const category = categoryMainNews[i];
-//       const date = dateMainNews[i];
-//       const title = titleMainNews[i];
-//       category.textContent = res[i].category.name;
-//       date.textContent = res[i].publish_date;
-//       title.textContent = res[i].title;
-//     }
-//     return getArticlesById(15, 15, idCategory);
-//   })
-//   .then((res) => {
-//     // Featured News
-//     for (let i = 0; i < categoryFeaturedNews.length; i++) {
-//       const category = categoryFeaturedNews[i];
-//       const date = dateFeaturedNews[i];
-//       const title = titleFeaturedNews[i];
-//       category.textContent = res[i].category.name;
-//       date.textContent = res[i].publish_date;
-//       title.textContent = res[i].title;
-//     }
-//     return getArticlesById(30, 15, idCategory);
-//   })
-//   .then((res) => {
-//     // last news primary
-//     for (let i = 0; i < categoryLatestNewsPrimary.length; i++) {
-//       const category = categoryLatestNewsPrimary[i];
-//       const date = dateLatestNewsPrimary[i];
-//       const title = titleLatestNewsPrimary[i];
-//       const desc = descLatestNewsPrimary[i];
-//       category.textContent = res[i].category.name;
-//       date.textContent = res[i].publish_date;
-//       title.textContent = res[i].title;
-//       desc.textContent = res[i].description;
-//     }
-//     return getArticlesById(45, 15, idCategory);
-//   })
-//   .then((res) => {
-//     // last news secondary
-//     for (let i = 0; i < categoryLatestNewsSecondary.length; i++) {
-//       const category = categoryLatestNewsSecondary[i];
-//       const date = dateLatestNewsSecondary[i];
-//       const title = titleLatestNewsSecondary[i];
-//       category.textContent = res[i].category.name;
-//       date.textContent = res[i].publish_date;
-//       title.textContent = res[i].title;
-//     }
-
-//     return getArticlesById(55, 15, idCategory);
-//   })
-//   .then((res) => {
-//     // last news third
-//     for (let i = 0; i < categoryLatestNewsThird.length; i++) {
-//       const category = categoryLatestNewsThird[i];
-//       const date = dateLatestNewsThird[i];
-//       const title = titleLatestNewsThird[i];
-//       category.textContent = res[i].category.name;
-//       date.textContent = res[i].publish_date;
-//       title.textContent = res[i].title;
-//     }
-//     return getArticlesById(5, 5, idCategory);
-//   })
-//   .then((res) => {
-//     // trending news
-//     for (let i = 0; i < categoryTrendingThird.length; i++) {
-//       const category = categoryTrendingThird[i];
-//       const date = dateTrendingThird[i];
-//       const title = titleTrendingThird[i];
-//       category.textContent = res[i].category.name;
-//       date.textContent = res[i].publish_date;
-//       title.textContent = res[i].title;
-//     }
-//     return getArticlesById(20, 3, idCategory);
-//   })
-//   .then((res) => {
-//     // popular
-//     for (let i = 0; i < categoryPopular.length; i++) {
-//       const category = categoryPopular[i];
-//       const date = datePopular[i];
-//       const title = titlePopular[i];
-//       category.textContent = res[i].category.name;
-//       date.textContent = res[i].publish_date;
-//       title.textContent = res[i].title;
-//     }
-//   });
--- news/js/main.js
+++ news/js/main.js
@@ -125,33 +125,33 @@
   });
 
   // Carousel item 4
-  $(".carousel-item-4").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    margin: 30,
-    dots: false,
-    loop: true,
-    nav: true,
-    navText: [
-      '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-      '<i class="fa fa-angle-right" aria-hidden="true"></i>',
-    ],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      576: {
-        items: 1,
-      },
-      768: {
-        items: 2,
-      },
-      992: {
-        items: 3,
-      },
-      1200: {
-        items: 4,
-      },
-    },
-  });
+  // $(".carousel-item-4").owlCarousel({
+  //   autoplay: true,
+  //   smartSpeed: 1000,
+  //   margin: 30,
+  //   dots: false,
+  //   loop: true,
+  //   nav: true,
+  //   navText: [
+  //     '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+  //     '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+  //   ],
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //     },
+  //     576: {
+  //       items: 1,
+  //     },
+  //     768: {
+  //       items: 2,
+  //     },
+  //     992: {
+  //       items: 3,
+  //     },
+  //     1200: {
+  //       items: 4,
+  //     },
+  //   },
+  // });
 })(jQuery);
