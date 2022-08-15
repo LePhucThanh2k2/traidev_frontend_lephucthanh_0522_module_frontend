a49f5c09f0f2ba312171d629b3d9e181c1ed4741
Linh Nguyen <nvlinh17041992@gmail.com>
2022-08-15 10:32:44 +0700

    fix pagination

--- news/js/category/pagination.js
+++ news/js/category/pagination.js
@@ -1,15 +1,11 @@
 // -------------------------------------------------------------------------------------------------------------------------------------
 $(document).ready(function () {
   const elePageItemList = document.querySelectorAll(".page");
-  let i = 0;
   let page = 1;
-  let firstIdx = 1;
-  let lastIdx = 4;
   const LIMIT = 14;
   const total = 64;
-  const TOTAL_PAGINATION = 3; // lastIdx - 1 | 0 1 2 3
-  const lastPage =
-    total % LIMIT === 0 ? total / LIMIT : Math.floor(total / LIMIT) + 1;
+  const pageRange = 4;
+  const lastPage = total % LIMIT === 0 ? total / LIMIT : Math.floor(total / LIMIT) + 1;
   /*
       page = 1 -> offset 0, limit = 5 -> 0
       page = 2 -> offset 5, limit = 5 -> 1
@@ -20,64 +16,37 @@ $(document).ready(function () {
                        2-5
       */
   renderItems(page);
-  renderPagination(firstIdx, lastIdx);
+  renderPaginationNew();
 
   // EVENT
-  $(document).on("click", ".btn-prev", function () {
-    if (i !== 0) {
-      i -= 1;
-    } else {
-      firstIdx -= 1;
-      lastIdx -= 1;
-    }
-    page -= 1;
-    handlerUrl(page);
-
-    if (page === 1) {
-      $(".btn-prev").addClass("disabled");
-    }
-    renderPagination(firstIdx, lastIdx);
-    renderItems(page);
-  });
-
   $(document).on("click", ".page", function (e) {
     const ele = e.target;
-
     page = parseInt(ele.innerText);
     handlerUrl(page);
 
-    if (page === 1) {
-      $(".btn-prev").addClass("disabled");
-    }
-    // Need debug
-    if (page === lastPage) {
-      console.log("zo");
-      $(".btn-next").addClass("disabled");
-    }
-    i = page - 1;
-
-    renderPagination(firstIdx, lastIdx);
+    if (page === 1) $(".btn-prev").addClass("disabled");
+    if (page === lastPage) $(".btn-next").addClass("disabled");
+    renderPaginationNew();
     renderItems(page);
   });
 
   $(document).on("click", ".btn-next", function () {
-    if (lastIdx === lastPage) {
-      i += 1;
-    } else {
-      firstIdx += 1;
-      lastIdx += 1;
-    }
     page += 1;
     handlerUrl(page);
-    renderPagination(firstIdx, lastIdx);
+    renderPaginationNew();
     renderItems(page);
-    if (page === lastPage) {
-      $(".btn-next").addClass("disabled");
-    }
+    if (page === lastPage) $(".btn-next").addClass("disabled");
+  });
+
+  $(document).on("click", ".btn-prev", function () {
+    page -= 1;
+    handlerUrl(page);
+    renderPaginationNew();
+    renderItems(page);
+    if (page === 1) $(".btn-prev").addClass("disabled");
   });
 
   //   Function
-
   function renderPagination(firstIdx, lastIdx) {
     let str = "";
     const classDisabled = page === 1 ? "disabled" : "";
@@ -92,6 +61,31 @@ $(document).ready(function () {
           <li class="page-item btn-next"><a class="page-link">Next</a></li>`
     );
   }
+
+  function renderPaginationNew() {
+    let str = "";
+    const classDisabledPrev = page === 1 ? "disabled" : "";
+    const classDisabledNext = page === lastPage ? "disabled" : "";
+    let end = page + pageRange - 1;
+    if (end > lastPage) end = lastPage;
+    start = end - pageRange + 1;
+
+    if (page === start && page !== 1) {
+      end--;
+      start--;
+    }
+
+    for (let i = start; i <= end; i++) {
+      const classActive = i === page ? "active" : "";
+      str += `<li class="page-item page ${classActive}" data-id="${i}"><a class="page-link">${i}</a></li>`;
+    }
+
+    $(".pagination").html(`
+      <li class="page-item btn-prev ${classDisabledPrev}"><a class="page-link" >Previous</a></li>
+        ${str}
+      <li class="page-item btn-next ${classDisabledNext}"><a class="page-link">Next</a></li>`);
+  }
+
   function renderItems(page = 1) {
     const OFFSET = (page - 1) * LIMIT;
     renderCategories(OFFSET, 14);
